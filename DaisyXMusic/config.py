bf9dbb6f185e1f42dfadcca7ddc1600b952122dd
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2022-11-04 11:58:38 +0000

    PyLint Auto Fixes

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -1,15 +1,18 @@
 from os import getenv
 
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "BABqyNt9m2718nDk_TrlLr5kC8kK3LJHWT8xkR0NjNN2-71920NWqm_5wBaPUtxpZFLcwN1zP7Cqj9qYI3Oc133oqdbZh2ETt5ugHQwIh66ghJkDu4LIAp937eh6BDUxd-PQwggMwTAn4kvxAReKA56E5liTY7BCXpOKjcvi3Mn01OzNJJi-kSYhhn5gAiSLe1MQI-HW2EAyk_yzRU0-AHpajPVMLKWL-HXfpPXyKrkl7-MuZVmvMeORdorl6e8LIAVISpVg1ar8fRVhkmNJhLyPPdiDmhbPR6jCJ7D4GHSIh1AodxXVN87JsXLS7GGvy6jv8hxSrUBPTZNLkLvp7ECUAAAAAU_mcXoA")
-BOT_TOKEN = getenv("BOT_TOKEN","5551102158:AAFfKwGvCD3Kvm-0ojn0zyRBRuAXD0mEtr8")
-BOT_NAME = getenv("BOT_NAME","◜𝑴𝑨𝑳𝑲𝒀 𝑀𝑈𝑆𝐼𝐶◞")
+SESSION_NAME = getenv(
+    "SESSION_NAME",
+    "BABqyNt9m2718nDk_TrlLr5kC8kK3LJHWT8xkR0NjNN2-71920NWqm_5wBaPUtxpZFLcwN1zP7Cqj9qYI3Oc133oqdbZh2ETt5ugHQwIh66ghJkDu4LIAp937eh6BDUxd-PQwggMwTAn4kvxAReKA56E5liTY7BCXpOKjcvi3Mn01OzNJJi-kSYhhn5gAiSLe1MQI-HW2EAyk_yzRU0-AHpajPVMLKWL-HXfpPXyKrkl7-MuZVmvMeORdorl6e8LIAVISpVg1ar8fRVhkmNJhLyPPdiDmhbPR6jCJ7D4GHSIh1AodxXVN87JsXLS7GGvy6jv8hxSrUBPTZNLkLvp7ECUAAAAAU_mcXoA",
+)
+BOT_TOKEN = getenv("BOT_TOKEN", "5551102158:AAFfKwGvCD3Kvm-0ojn0zyRBRuAXD0mEtr8")
+BOT_NAME = getenv("BOT_NAME", "◜𝑴𝑨𝑳𝑲𝒀 𝑀𝑈𝑆𝐼𝐶◞")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DaisyXupdates")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/8e356b44256d7e2bdbc9b.jpg")
 admins = {}
 API_ID = int(getenv("API_ID", "18340569"))
 API_HASH = getenv("API_HASH", "ffb52ac8ab95e74de55b1cc8bb455eea")
-BOT_USERNAME = getenv("BOT_USERNAME","modea_bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "modea_bot")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "—͟͟͞͞𖣘𝑬𝒍 𝑱𝑶𝑲𝑬𝑹—͟͟͞͞𖣘")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DaisySupport_Official")
 PROJECT_NAME = getenv("PROJECT_NAME", "DaisyXMusic v6")
